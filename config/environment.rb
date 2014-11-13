eef0865f28d28e7de3a368a1a0744699156673fe
vanessadean <vanessa.dean@gmail.com>
2014-11-13 16:21:35 -0500

    Edits

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,4 @@
-require 'sinatra'
-require 'pry'
 require 'bundler'
-require 'bcrypt'
 Bundler.require
 
 require './app/models/tweet'
@@ -9,13 +6,3 @@ require './app/models/user'
 require './app/models/follower'
 require './app/models/following'
 
-configure do
-  set :public_folder, 'public'
-  set :views, 'app/views'
-  enable :sessions
-  set :session_secret, 'fwitter'
-end
-
-configure :development do
-  set :database, "sqlite3:///db/database.db"
-end
